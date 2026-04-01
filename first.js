2bee180e35955c003c762086b1f75784eb01ab96
ankitanmol26 <ankitanmol26@gmail.com>
2026-04-01 21:47:12 +0530

    js

---
+++ first.js
@@ -0,0 +1,27 @@
+// alert("alert from first.js")
+let a = 1;
+let b = 2;
+console.log( "a+b" , a+b);
+console.log( "a-b" , a-b );
+
+let mode = "dark";
+if (mode == "dark"){
+    console.log("dark");
+
+}else{
+    console.log("light");
+}
+
+let age = 20;
+if (age >= 18) {
+    console.log("adult");
+} else {
+    console.log("child");
+}
+
+let num = prompt("enter your number")
+if (num % 5 === 0){
+    console.log("true");
+}else{
+    console.log("false");
+}
